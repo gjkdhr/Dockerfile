a1d146b0377ba6fc15cf480a1a609c27cd8c2031
gjkdhr <gjkdhr@163.com>
2015-12-08 19:33:50 +0800

    modify the config_mysql.sh

--- Dockerfile_mysqld/config_mysql.sh
+++ Dockerfile_mysqld/config_mysql.sh
@@ -13,9 +13,8 @@ function config_passwd(){
 	/usr/bin/mysqladmin -u root password "redhat"
 	#create the database testdb,the work enviorment .
 	mysql -uroot -predhat -e "CREATE DATABASE testdb;"
-	mysql -uroot -predhat -e "GRANT ALL PRIVILEGES ON *.* TO 'testdb'@'localhost' IDENTIFIED BY 'testdb';FLUSH PRIVILEGES;" 
-	mysql -uroot -predhat -e "GRANT ALL PRIVILEGES ON testdb.* TO 'testdb'@'%' IDENTIFIED BY 'testdbadmin'; FLUSH PRIVILEGES;"
-	mysql -uroot -predhat -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'dbadmin';FLUSH PRIVILEGES;"
+	mysql -uroot -predhat -e "GRANT ALL PRIVILEGES ON testdb.* TO 'testdb'@'%' IDENTIFIED BY 'testdbadmin' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+	mysql -uroot -predhat -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'dbadmin' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 	mysql -uroot -predhat -e "select user, host FROM mysql.user;"
 	killall mysqld
 
